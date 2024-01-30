74d1bb6394142f40d03ce5357b1ba70c77a95631
Gonzalo Perea <gonzalopereabartra@gmail.com>
2024-01-30 15:16:03 -0500

    Carousel Initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,43 @@
 import CalculatePrice from "@/components/calculate-price";
 import Image from "next/image";
 
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
+import test1 from '/TestImages/test1.jpeg';
+import test2 from '/TestImages/test2.jpeg';
+import test3 from '/TestImages/test3.jpeg';
+
+
 export default function Home() {
 
   return (
-    <Carousel className="rounded-xl">
-      <img src="test.png" alt="test" className="h-full w-full object-cover" />
-      <img src="test.png" alt="test" className="h-full w-full object-cover" />
-      <img src="test.png" alt="test" className="h-full w-full object-cover" />
+    <>
+    <Carousel opts={{
+        align: "center",
+        loop: true,
+      }} className="w-screen">
 
-      <CalculatePrice />
+      
+
+      <CarouselContent>
+        <CarouselItem>
+          <Image src={test1} width={700} height={700} alt="Test Image" className="object-cover aspect-auto h-full w-screen"/>
+        </CarouselItem>
+        <CarouselItem>
+          <Image src={test2} width={500} height={500} alt="Test Image" className="object-cover aspect-auto h-full w-screen"/>
+        </CarouselItem>
+        <CarouselItem>
+          <Image src={test3} width={500} height={500} alt="Test Image" className="object-cover aspect-auto h-full w-screen"/>
+        </CarouselItem>
+      </CarouselContent>
+
     </Carousel>
+    </>
   );
 }
