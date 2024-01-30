30b2c8937692447a6739d64c101a5c9b8cd332b5
Gonzalo Perea <gonzalopereabartra@gmail.com>
2024-01-30 14:21:45 -0500

    Test Images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
 
+import { Carousel } from "@material-tailwind/react";
+
 export default function Home() {
-  
+
   return (
-    <div>
-    </div>
+    <Carousel className="rounded-xl">
+      <img src="test.png" alt="test" className="h-full w-full object-cover" />
+      <img src="test.png" alt="test" className="h-full w-full object-cover" />
+      <img src="test.png" alt="test" className="h-full w-full object-cover" />
+    </Carousel>
   );
 }
