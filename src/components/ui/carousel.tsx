e89216de27a88e4283682ec267492bdb48f860d4
Gonzalo Perea <gonzalopereabartra@gmail.com>
2024-01-30 18:13:57 -0500

    Fine Tuned scrolling

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -137,7 +137,7 @@ const Carousel = React.forwardRef<
         <div
           ref={ref}
           onKeyDownCapture={handleKeyDown}
-          className={cn("relative", className)}
+          className={cn("relative ", className)}
           role="region"
           aria-roledescription="carousel"
           {...props}
@@ -162,7 +162,7 @@ const CarouselContent = React.forwardRef<
         ref={ref}
         className={cn(
           "flex",
-          orientation === "horizontal" ? "-ml-4" : "-mt-4 flex-col",
+          orientation === "horizontal" ? "ml-0" : "-mt-4 flex-col",
           className
         )}
         {...props}
@@ -185,7 +185,7 @@ const CarouselItem = React.forwardRef<
       aria-roledescription="slide"
       className={cn(
         "min-w-0 shrink-0 grow-0 basis-full",
-        orientation === "horizontal" ? "pl-4" : "pt-4",
+        orientation === "horizontal" ? "px-0 mx-0" : "pt-4",
         className
       )}
       {...props}
